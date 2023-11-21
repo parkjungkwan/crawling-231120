3b1a694b39d50fdf689f4009d86fc6cc0bb9f1cf
parkjungkwan <pakjkwan@gmail.com>
2023-11-21 15:19:33 +0900

    Translater commit

--- 9. 영어로된 문서를 한글로 자동번역/main9-1.py
+++ 9. 영어로된 문서를 한글로 자동번역/main9-1.py
@@ -1,11 +1,30 @@
 import googletrans 
 # pip install googletrans==4.0.0-rc1
-translator = googletrans.Translator() 
 
-str1 = "행복하세요" 
-result1 = translator.translate(str1, dest='en', src='auto') 
-print(f"행복하세요 => {result1.text}") 
+class Translator:
+    def __init__(self):
+        self.translator = googletrans.Translator()
+        self.text = ''
 
-str2 = "I am happy" 
-result2 = translator.translate(str2, dest='ko', src='en') 
-print(f"I am happy => {result2.text}")
+    def set_text(self, text):
+        self.text = text
+
+    def en_to_kr(self):
+
+        result2 = self.translator.translate(self.text, dest='ko', src='en')
+        return result2
+        print(f"I am happy => {result2.text}")
+
+    def ko_to_en(self):
+        result1 = self.translator.translate(self.text, dest='en', src='auto')
+        return result1
+        print(f"행복하세요 => {result1.text}")
+
+if __name__ == '__main__':
+
+
+
+
+
+
+
